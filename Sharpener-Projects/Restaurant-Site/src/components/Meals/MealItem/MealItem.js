1e0e3f49c5fe14d0ae114fd4ff26273ae849ea31
Vaibhav <vaibhavjam86@gmail.com>
2024-05-24 10:13:47 +0530

    Updated mealItem file

--- Sharpener-Projects/Restaurant-Site/src/components/Meals/MealItem/MealItem.js
+++ Sharpener-Projects/Restaurant-Site/src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,55 @@
+// import { useContext } from 'react';
+// import classes from './MealItem.module.css';
+// import MealItemForm from './MealItemForm';
+// import CartContext from '../../../store/CartContext';
+
+// const MealItem = (props) => {
+//   const cartCtx = useContext(CartContext);
+//     const price = `$${props.price.toFixed(2)}`;
+
+//     const onAddToCartHandler = amount => {
+//       cartCtx.addItem({
+//         id: props.id,
+//         name: props.name,
+//         amount: amount,
+//         price: props.price
+//       })
+//     };
+
+//     return (
+//         <li className={classes.meal}>
+//           <div>
+//             <h3>{props.name}</h3>
+//             <div className={classes.description}>{props.description}</div>
+//             <div className={classes.price}>{price}</div>
+//           </div>
+//           <div>
+//             <MealItemForm  onAddToCart={onAddToCartHandler} />
+//           </div>
+//         </li>
+//       );
+// };
+
+// export default MealItem;
+
+import { useContext } from "react";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
+
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+
+  const onAddToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
+
   return (
     <li className={classes.meal}>
       <div>
@@ -10,7 +58,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm onAddToCart={onAddToCartHandler} />
       </div>
     </li>
   );

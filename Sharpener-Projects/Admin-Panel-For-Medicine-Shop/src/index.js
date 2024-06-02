d24cd4feca4e442cb1930945d1166a9d5502192c
Vaibhav <vaibhavjam86@gmail.com>
2024-06-02 11:25:05 +0530

    index.js file

---
+++ Sharpener-Projects/Admin-Panel-For-Medicine-Shop/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);

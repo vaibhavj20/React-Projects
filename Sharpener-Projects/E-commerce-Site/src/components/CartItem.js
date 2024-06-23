fd07f090daca755245d33327d7455e4349117aa5
Vaibhav <vaibhavjam86@gmail.com>
2024-06-23 19:45:39 +0530

    Description of each cart

---
+++ Sharpener-Projects/E-commerce-Site/src/components/CartItem.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button, FormControl } from "react-bootstrap";
+
+const CartItem = ({ item, onRemove, onQuantityChange }) => {
+  const handleQuantityChange = (event) => {
+    const quantity = parseInt(event.target.value, 10);
+    if (!isNaN(quantity) && quantity > 0) {
+      onQuantityChange(quantity);
+    }
+  };
+
+  return (
+    <tr>
+      <td>
+        <img src={item.imageUrl} alt={item.title} className="cart-item-image" />
+        {item.title}
+      </td>
+      <td>${item.price.toFixed(2)}</td>
+      <td>
+        <FormControl
+          type="number"
+          value={item.quantity}
+          onChange={handleQuantityChange}
+          style={{ width: "60px", display: "inline-block" }}
+        />
+      </td>
+      <td>
+        <Button variant="danger" onClick={onRemove}>
+          REMOVE
+        </Button>
+      </td>
+    </tr>
+  );
+};
+
+export default CartItem;

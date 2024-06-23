c24e59a6366ccbfeab265cc1dbe6fc268b94fee7
Vaibhav <vaibhavjam86@gmail.com>
2024-06-23 09:21:04 +0530

    Header file contains Heading

---
+++ Sharpener-Projects/E-commerce-Site/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="App-header bg-secondary text-white text-center py-5">
+      <h1>The Generics</h1>
+    </header>
+  );
+};
+
+export default Header;

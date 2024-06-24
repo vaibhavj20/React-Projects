d3f173fecbeb55da95e9f2f95daea90c6c4812b4
Vaibhav <vaibhavjam86@gmail.com>
2024-06-24 19:09:43 +0530

    wrap with router

--- Sharpener-Projects/E-commerce-Site/src/index.js
+++ Sharpener-Projects/E-commerce-Site/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { CartProvider } from "./context/CartContext";
 
 ReactDOM.render(
   <CartProvider>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </CartProvider>,
   document.getElementById("root")
 );

2ec95c54ccf0ea5b25268505034711110b748c64
Vaibhav <vaibhavjam86@gmail.com>
2024-06-24 19:07:11 +0530

    added routes

--- Sharpener-Projects/E-commerce-Site/src/App.js
+++ Sharpener-Projects/E-commerce-Site/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import AppNavbar from "./components/AppNavbar";
 import ProductList from "./components/ProductList";
 import productsArr from "./components/Products";
+import About from "./components/About";
 import "./App.css";
 
 function App() {
@@ -9,8 +11,18 @@ function App() {
     <div>
       <AppNavbar />
       <div className="container mt-4">
-        <h1 className="text-center mb-4">The Generics</h1>
-        <ProductList products={productsArr} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <h1 className="text-center mb-4">The Generics</h1>
+                <ProductList products={productsArr} />
+              </>
+            }
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
     </div>
   );

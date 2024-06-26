f1121d8f2e10a562ca3c4e4ac2c8f0bef7d58373
Vaibhav <vaibhavjam86@gmail.com>
2024-06-26 17:54:16 +0530

    Added Loader while fetching movies

--- Sharpener-Projects/Movies/src/App.js
+++ Sharpener-Projects/Movies/src/App.js
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import Loader from "./components/Loader";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMovieHandler() {
+    setIsLoading(true);
     //fetched api
     const response = await fetch("https://swapi.dev/api/films");
     const data = await response.json();
@@ -20,6 +23,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -28,7 +32,9 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {/* {!isLoading && movies.length === 0 && <p>No Movies Found</p>} */}
+        {isLoading && <Loader />}
       </section>
     </React.Fragment>
   );

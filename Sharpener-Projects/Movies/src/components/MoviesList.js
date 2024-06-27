fa1b54b2ddc6b7ff62f2f5f508f76e2e7cf547ee
Vaibhav <vaibhavjam86@gmail.com>
2024-06-27 09:10:11 +0530

    updated

--- Sharpener-Projects/Movies/src/components/MoviesList.js
+++ Sharpener-Projects/Movies/src/components/MoviesList.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React from "react";
+import Movie from "./Movie";
+import classes from "./MoviesList.module.css";
 
-import Movie from './Movie';
-import classes from './MoviesList.module.css';
-
-const MovieList = (props) => {
+const MoviesList = (props) => {
   return (
-    <ul className={classes['movies-list']}>
+    <ul className={classes["movies-list"]}>
       {props.movies.map((movie) => (
         <Movie
           key={movie.id}
+          id={movie.id}
           title={movie.title}
           releaseDate={movie.releaseDate}
           openingText={movie.openingText}
+          onDeleteMovie={props.onDeleteMovie}
         />
       ))}
     </ul>
   );
 };
 
-export default MovieList;
+export default MoviesList;

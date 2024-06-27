c4e29e65bc5c6a5b077daef370bee3af4fd4d7ba
Vaibhav <vaibhavjam86@gmail.com>
2024-06-27 09:09:43 +0530

    added delete btn to delete added movie from database

--- Sharpener-Projects/Movies/src/components/Movie.js
+++ Sharpener-Projects/Movies/src/components/Movie.js
@@ -1,6 +1,5 @@
-import React from 'react';
-
-import classes from './Movie.module.css';
+import React from "react";
+import classes from "./Movie.module.css";
 
 const Movie = (props) => {
   return (
@@ -8,6 +7,19 @@ const Movie = (props) => {
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <button
+        style={{
+          marginTop: "0.5rem",
+          padding: "0.5rem 1rem",
+          font: "inherit",
+          border: "1px solid #ff2058",
+          color: "white",
+          background: "red",
+        }}
+        onClick={() => props.onDeleteMovie(props.id)}
+      >
+        Delete
+      </button>
     </li>
   );
 };

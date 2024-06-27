c389f4d3c527a588ca03f1f5a3357ddd2be72b18
Vaibhav <vaibhavjam86@gmail.com>
2024-06-27 16:22:58 +0530

    Implement the Products Page

--- Sharpener-Projects/E-commerce-Site/src/App.js
+++ Sharpener-Projects/E-commerce-Site/src/App.js
@@ -1,36 +1,67 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import AppNavbar from "./components/AppNavbar";
+// import ProductList from "./components/ProductList";
+// import ContactUs from "./components/ContactUs";
+// import About from "./components/About";
+// import productsArr from "./components/Products";
+// import Home from "./components/Home";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div>
+//       <AppNavbar />
+//       <div className="container mt-4">
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route
+//             path="/store"
+//             element={<ProductList products={productsArr} />}
+//           />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/contact" element={<ContactUs />} />
+//         </Routes>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppNavbar from "./components/AppNavbar";
 import ProductList from "./components/ProductList";
 import ContactUs from "./components/ContactUs";
 import About from "./components/About";
-import productsArr from "./components/Products";
+import Home from "./components/Home";
+import ProductDetails from "./components/ProductDetails"; // New component
+import { CartProvider } from "./context/CartContext";
+import productsArr from "./components/Products"; // Your products array
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <CartProvider>
       <AppNavbar />
       <div className="container mt-4">
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <h1 className="text-center mb-4">The Generics</h1>
-                <ProductList products={productsArr} />
-              </>
-            }
-          />
+          <Route path="/" element={<Home />} />
           <Route
             path="/store"
             element={<ProductList products={productsArr} />}
           />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<ContactUs />} />
+          <Route
+            path="/product/:id"
+            element={<ProductDetails products={productsArr} />}
+          />{" "}
+          {/* New route */}
         </Routes>
       </div>
-    </div>
+    </CartProvider>
   );
 }
 

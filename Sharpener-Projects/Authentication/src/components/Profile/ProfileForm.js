43b93efd6ec90b699174e4e6acc1f559257ee84f
Vaibhav <vaibhavjam86@gmail.com>
2024-06-29 09:44:16 +0530

    profile file

---
+++ Sharpener-Projects/Authentication/src/components/Profile/ProfileForm.js
@@ -0,0 +1,44 @@
+import classes from "./ProfileForm.module.css";
+import { useContext, useRef } from "react";
+import AuthContext from "../../store/auth-context";
+import { useHistory } from "react-router-dom";
+
+const ProfileForm = () => {
+  const history = useHistory();
+  const newPasswordTnputRef = useRef();
+  const authCtx = useContext(AuthContext);
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const entredNewPassword = newPasswordTnputRef.current.value;
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyB-ST5yXnOyqZOwkiyy8z3gggETofw6w_I",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: entredNewPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      history.replace("/");
+    });
+  };
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" minLength="7" />
+      </div>
+      <div className={classes.action}>
+        <button>Change Password</button>
+      </div>
+    </form>
+  );
+};
+
+export default ProfileForm;

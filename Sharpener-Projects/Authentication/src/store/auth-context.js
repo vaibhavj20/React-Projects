d1be9ae0a701c2ab7a703f1f30f1322404365f0f
Vaibhav <vaibhavjam86@gmail.com>
2024-07-23 10:19:10 +0530

    context file

--- Sharpener-Projects/Authentication/src/store/auth-context.js
+++ Sharpener-Projects/Authentication/src/store/auth-context.js
@@ -1,5 +1,41 @@
-import React from "react";
-import { useState } from "react";
+// import React from "react";
+// import { useState } from "react";
+// const AuthContext = React.createContext({
+//   token: "",
+//   isLoggedIn: false,
+//   login: (token) => {},
+//   logout: () => {},
+// });
+
+// export const AuthContextProvider = (props) => {
+//   const [token, setToken] = useState(null);
+
+//   const userLoggedIn = !!token;
+//   const loginHandler = () => {
+//     setToken(token);
+//   };
+//   const logoutHandler = () => {
+//     setToken(null);
+//   };
+
+//   const contextValue = {
+//     token: token,
+//     isLoggedIn: userLoggedIn,
+//     login: loginHandler,
+//     logout: logoutHandler,
+//   };
+
+//   return (
+//     <AuthContext.Provider value={contextValue}>
+//       {props.children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+// export default AuthContext;
+
+import React, { useState, useEffect } from "react";
+
 const AuthContext = React.createContext({
   token: "",
   isLoggedIn: false,
@@ -10,12 +46,23 @@ const AuthContext = React.createContext({
 export const AuthContextProvider = (props) => {
   const [token, setToken] = useState(null);
 
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
+
   const userLoggedIn = !!token;
-  const loginHandler = () => {
+
+  const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem("token", token);
   };
+
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
   };
 
   const contextValue = {

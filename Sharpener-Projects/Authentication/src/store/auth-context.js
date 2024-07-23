493ba1c6e5dd3bb5848c11cd639cb736c157f80a
Vaibhav <vaibhavjam86@gmail.com>
2024-07-23 10:56:39 +0530

    Auto logout

--- Sharpener-Projects/Authentication/src/store/auth-context.js
+++ Sharpener-Projects/Authentication/src/store/auth-context.js
@@ -34,7 +34,7 @@
 
 // export default AuthContext;
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 const AuthContext = React.createContext({
   token: "",
@@ -48,8 +48,18 @@ export const AuthContextProvider = (props) => {
 
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
-    if (storedToken) {
-      setToken(storedToken);
+    const storedExpirationDate = localStorage.getItem("expirationDate");
+
+    if (storedToken && storedExpirationDate) {
+      const remainingTime =
+        new Date(storedExpirationDate).getTime() - new Date().getTime();
+      if (remainingTime <= 0) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("expirationDate");
+      } else {
+        setToken(storedToken);
+        setTimeout(logoutHandler, remainingTime);
+      }
     }
   }, []);
 
@@ -57,13 +67,17 @@ export const AuthContextProvider = (props) => {
 
   const loginHandler = (token) => {
     setToken(token);
+    const expirationDate = new Date(new Date().getTime() + 5 * 60 * 1000); // 5 minutes from now
     localStorage.setItem("token", token);
+    localStorage.setItem("expirationDate", expirationDate.toISOString());
+    setTimeout(logoutHandler, 5 * 60 * 1000);
   };
 
-  const logoutHandler = () => {
+  const logoutHandler = useCallback(() => {
     setToken(null);
     localStorage.removeItem("token");
-  };
+    localStorage.removeItem("expirationDate");
+  }, []);
 
   const contextValue = {
     token: token,

2582221549a801b82b082d3e438c9de946f39fa6
Vaibhav <vaibhavjam86@gmail.com>
2024-07-23 09:40:22 +0530

    Profile form

--- Sharpener-Projects/Authentication/src/components/Profile/ProfileForm.js
+++ Sharpener-Projects/Authentication/src/components/Profile/ProfileForm.js
@@ -1,15 +1,61 @@
-import classes from "./ProfileForm.module.css";
+// import classes from "./ProfileForm.module.css";
+// import { useContext, useRef } from "react";
+// import AuthContext from "../../store/auth-context";
+// import { useHistory } from "react-router-dom";
+
+// const ProfileForm = () => {
+//   const history = useHistory();
+//   const newPasswordTnputRef = useRef();
+//   const authCtx = useContext(AuthContext);
+//   const submitHandler = (event) => {
+//     event.preventDefault();
+//     const entredNewPassword = newPasswordTnputRef.current.value;
+
+//     fetch(
+//       "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyB-ST5yXnOyqZOwkiyy8z3gggETofw6w_I",
+//       {
+//         method: "POST",
+//         body: JSON.stringify({
+//           idToken: authCtx.token,
+//           password: entredNewPassword,
+//           returnSecureToken: false,
+//         }),
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     ).then((res) => {
+//       history.replace("/");
+//     });
+//   };
+//   return (
+//     <form className={classes.form} onSubmit={submitHandler}>
+//       <div className={classes.control}>
+//         <label htmlFor="new-password">New Password</label>
+//         <input type="password" id="new-password" minLength="7" />
+//       </div>
+//       <div className={classes.action}>
+//         <button>Change Password</button>
+//       </div>
+//     </form>
+//   );
+// };
+
+// export default ProfileForm;
+
 import { useContext, useRef } from "react";
 import AuthContext from "../../store/auth-context";
 import { useHistory } from "react-router-dom";
+import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
   const history = useHistory();
-  const newPasswordTnputRef = useRef();
+  const newPasswordInputRef = useRef();
   const authCtx = useContext(AuthContext);
+
   const submitHandler = (event) => {
     event.preventDefault();
-    const entredNewPassword = newPasswordTnputRef.current.value;
+    const enteredNewPassword = newPasswordInputRef.current.value;
 
     fetch(
       "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyB-ST5yXnOyqZOwkiyy8z3gggETofw6w_I",
@@ -17,7 +63,7 @@ const ProfileForm = () => {
         method: "POST",
         body: JSON.stringify({
           idToken: authCtx.token,
-          password: entredNewPassword,
+          password: enteredNewPassword,
           returnSecureToken: false,
         }),
         headers: {
@@ -28,11 +74,18 @@ const ProfileForm = () => {
       history.replace("/");
     });
   };
+
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor="new-password">New Password</label>
-        <input type="password" id="new-password" minLength="7" />
+        <input
+          type="password"
+          id="new-password"
+          minLength="7"
+          ref={newPasswordInputRef}
+          required
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>

e0492c1056f5b4c7ff76e6b71b5487f67b02b560
Vaibhav <vaibhavjam86@gmail.com>
2024-07-23 09:39:48 +0530

    Auth form

--- Sharpener-Projects/Authentication/src/components/Auth/AuthForm.js
+++ Sharpener-Projects/Authentication/src/components/Auth/AuthForm.js
@@ -1,3 +1,114 @@
+// import { useState, useRef, useContext } from "react";
+// import classes from "./AuthForm.module.css";
+// import AuthContext from "../../store/auth-context";
+// import { useHistory } from "react-router-dom";
+
+// const AuthForm = () => {
+//   const history = useHistory();
+//   const emailInputRef = useRef();
+//   const passwordInputRef = useRef();
+
+//   const authCtx = useContext(AuthContext);
+
+//   const [isLogin, setIsLogin] = useState(true);
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   const switchAuthModeHandler = () => {
+//     setIsLogin((prevState) => !prevState);
+//   };
+
+//   const submitHandler = (event) => {
+//     event.preventDefault();
+
+//     const enteredEmail = emailInputRef.current.value;
+//     const enteredPassword = passwordInputRef.current.value;
+
+//     setIsLoading(true);
+//     let url;
+//     if (isLogin) {
+//       // Add login functionality
+
+//       url =
+//         "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB-ST5yXnOyqZOwkiyy8z3gggETofw6w_I";
+//     } else {
+//       url =
+//         "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB-ST5yXnOyqZOwkiyy8z3gggETofw6w_I";
+//     }
+
+//     fetch(url, {
+//       method: "POST",
+//       body: JSON.stringify({
+//         email: enteredEmail,
+//         password: enteredPassword,
+//         returnSecureToken: true,
+//       }),
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     })
+//       .then((res) => {
+//         setIsLoading(false);
+//         if (res.ok) {
+//           // Handle success
+//           return res.json();
+//         } else {
+//           res.json().then((data) => {
+//             // Show an error
+//             let errorMessage = "Authentication Failed!";
+//             // if (data && data.error && data.error.message) {
+//             //   errorMessage = data.error.message;
+//             // }
+
+//             throw new Error(errorMessage);
+//           });
+//         }
+//       })
+//       .then((data) => {
+//         authCtx.login(data.idToken);
+//         history.replace("/");
+//       })
+//       .catch((err) => {
+//         alert(err.message);
+//       });
+//   };
+
+//   return (
+//     <section className={classes.auth}>
+//       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+//       <form onSubmit={submitHandler}>
+//         <div className={classes.control}>
+//           <label htmlFor="email">Your Email</label>
+//           <input type="email" id="email" required ref={emailInputRef} />
+//         </div>
+//         <div className={classes.control}>
+//           <label htmlFor="password">Your Password</label>
+//           <input
+//             type="password"
+//             id="password"
+//             required
+//             ref={passwordInputRef}
+//           />
+//         </div>
+//         <div className={classes.actions}>
+//           {!isLoading && (
+//             <button>{isLogin ? "Login" : "Create Account"}</button>
+//           )}
+//           {isLoading && <p>Sending Request</p>}
+//           <button
+//             type="button"
+//             className={classes.toggle}
+//             onClick={switchAuthModeHandler}
+//           >
+//             {isLogin ? "Create new account" : "Login with existing account"}
+//           </button>
+//         </div>
+//       </form>
+//     </section>
+//   );
+// };
+
+// export default AuthForm;
+
 import { useState, useRef, useContext } from "react";
 import classes from "./AuthForm.module.css";
 import AuthContext from "../../store/auth-context";
@@ -26,8 +137,6 @@ const AuthForm = () => {
     setIsLoading(true);
     let url;
     if (isLogin) {
-      // Add login functionality
-
       url =
         "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB-ST5yXnOyqZOwkiyy8z3gggETofw6w_I";
     } else {
@@ -49,15 +158,13 @@ const AuthForm = () => {
       .then((res) => {
         setIsLoading(false);
         if (res.ok) {
-          // Handle success
           return res.json();
         } else {
-          res.json().then((data) => {
-            // Show an error
-            let errorMessage = "Authentication Failed!";
-            // if (data && data.error && data.error.message) {
-            //   errorMessage = data.error.message;
-            // }
+          return res.json().then((data) => {
+            let errorMessage = "Authentication failed!";
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
 
             throw new Error(errorMessage);
           });
@@ -93,7 +200,7 @@ const AuthForm = () => {
           {!isLoading && (
             <button>{isLogin ? "Login" : "Create Account"}</button>
           )}
-          {isLoading && <p>Sending Request</p>}
+          {isLoading && <p>Sending Request...</p>}
           <button
             type="button"
             className={classes.toggle}

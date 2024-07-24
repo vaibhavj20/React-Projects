8023be7ef4875f315685542729ff3c482f0f9e9d
Vaibhav <vaibhavjam86@gmail.com>
2024-07-24 22:57:07 +0530

    modified app-added private route

--- Sharpener-Projects/E-commerce-Site/src/App.js
+++ Sharpener-Projects/E-commerce-Site/src/App.js
@@ -1,34 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import AppNavbar from "./components/AppNavbar";
-// import ProductList from "./components/ProductList";
-// import ContactUs from "./components/ContactUs";
-// import About from "./components/About";
-// import productsArr from "./components/Products";
-// import Home from "./components/Home";
-// import "./App.css";
-
-// function App() {
-//   return (
-//     <div>
-//       <AppNavbar />
-//       <div className="container mt-4">
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route
-//             path="/store"
-//             element={<ProductList products={productsArr} />}
-//           />
-//           <Route path="/about" element={<About />} />
-//           <Route path="/contact" element={<ContactUs />} />
-//         </Routes>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppNavbar from "./components/AppNavbar";
@@ -36,33 +5,47 @@ import ProductList from "./components/ProductList";
 import ContactUs from "./components/ContactUs";
 import About from "./components/About";
 import Home from "./components/Home";
-import ProductDetails from "./components/ProductDetails"; // New component
+import ProductDetails from "./components/ProductDetails";
 import { CartProvider } from "./context/CartContext";
-import productsArr from "./components/Products"; // Your products array
+import productsArr from "./components/Products";
 import "./App.css";
 import AuthForm from "./components/AuthForm";
+import { AuthContextProvider } from "./context/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-    <CartProvider>
-      <AppNavbar />
-      <div className="container mt-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/store"
-            element={<ProductList products={productsArr} />}
-          />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<AuthForm />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route
-            path="/product/:id"
-            element={<ProductDetails products={productsArr} />}
-          />
-        </Routes>
-      </div>
-    </CartProvider>
+    <AuthContextProvider>
+      <CartProvider>
+        <Router>
+          <AppNavbar />
+          <div className="container mt-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/store"
+                element={
+                  <PrivateRoute>
+                    <ProductList products={productsArr} />
+                  </PrivateRoute>
+                }
+              />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<AuthForm />} />
+              <Route path="/contact" element={<ContactUs />} />
+              <Route
+                path="/product/:id"
+                element={
+                  <PrivateRoute>
+                    <ProductDetails products={productsArr} />
+                  </PrivateRoute>
+                }
+              />
+            </Routes>
+          </div>
+        </Router>
+      </CartProvider>
+    </AuthContextProvider>
   );
 }
 

bb4b30434c542d8e92f53648595c4c47dc7e3f9b
Vaibhav <vaibhavjam86@gmail.com>
2024-07-26 11:39:12 +0530

    modified product list

--- Sharpener-Projects/E-commerce-Site/src/components/ProductList.js
+++ Sharpener-Projects/E-commerce-Site/src/components/ProductList.js
@@ -4,43 +4,6 @@ import { Link } from "react-router-dom";
 import CartContext from "../context/CartContext";
 import styles from "./ProductList.module.css";
 
-// const ProductList = ({ products }) => {
-//   const { addItemToCart } = useContext(CartContext);
-
-//   return (
-//     <Row>
-//       {products.map((product, index) => (
-//         <Col key={index} xs={12} md={6} lg={4} className="mb-4">
-//           <Link to={`/product/${index}`} className={styles.productLink}>
-//             <Card className={styles.productCard}>
-//               <Card.Img
-//                 variant="top"
-//                 src={product.imageUrl}
-//                 className={styles.productImage}
-//               />
-//               <Card.Body>
-//                 <Card.Title>{product.title}</Card.Title>
-//                 <Card.Text>${product.price.toFixed(2)}</Card.Text>
-//                 <Button
-//                   className={styles.addToCartButton}
-//                   onClick={(e) => {
-//                     e.preventDefault();
-//                     addItemToCart(product);
-//                   }}
-//                 >
-//                   Add to Cart
-//                 </Button>
-//               </Card.Body>
-//             </Card>
-//           </Link>
-//         </Col>
-//       ))}
-//     </Row>
-//   );
-// };
-
-// export default ProductList;
-
 const ProductList = ({ products }) => {
   const { addItemToCart } = useContext(CartContext);
 

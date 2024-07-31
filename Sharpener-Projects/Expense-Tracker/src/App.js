bf2a4ae962db02f24f8a482f3d65a576d1480a98
Vaibhav <vaibhavjam86@gmail.com>
2024-07-31 11:27:26 +0530

    Added Signup and Login Form with router

---
+++ Sharpener-Projects/Expense-Tracker/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Signup from "./forms/SignUp";
+import Login from "./forms/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;

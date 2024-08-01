ea047ce25f6c7deccce9cf234a9923e6e1dddf45
Vaibhav <vaibhavjam86@gmail.com>
2024-08-01 09:45:19 +0530

    Added toast container and route for home page

--- Sharpener-Projects/Expense-Tracker/src/App.js
+++ Sharpener-Projects/Expense-Tracker/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import Signup from "./forms/SignUp";
 import Login from "./forms/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -10,7 +13,9 @@ function App() {
         <Route path="/" element={<Signup />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
+      <ToastContainer />
     </BrowserRouter>
   );
 }

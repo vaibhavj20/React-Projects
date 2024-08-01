0713f11db1f1d7d257eb17e91e9e955686cfdbfd
Vaibhav <vaibhavjam86@gmail.com>
2024-08-01 09:42:59 +0530

    Added Home page

---
+++ Sharpener-Projects/Expense-Tracker/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="card">
+        <h2>Welcome to Expense Tracker</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

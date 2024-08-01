7b4b05bab6c33182639dcf9cb84a8470e0bc5848
Vaibhav <vaibhavjam86@gmail.com>
2024-08-01 09:47:38 +0530

    Added toasts for handling errors

--- Sharpener-Projects/Expense-Tracker/src/forms/Login.js
+++ Sharpener-Projects/Expense-Tracker/src/forms/Login.js
@@ -1,16 +1,52 @@
 import React, { useState } from "react";
-
 import "../style.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../auth/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const token = await userCredential.user.getIdToken();
+      localStorage.setItem("token", token);
+      console.log("Login Successfully");
+      toast.success("Login Successfully");
+      navigate("/home");
+    } catch (error) {
+      console.log(error);
+      switch (error.code) {
+        case "auth/wrong-password":
+          toast.error("Invalid password. Please try again.");
+          break;
+        case "auth/user-not-found":
+          toast.error("No account found with this email. Please sign up.");
+          break;
+        case "auth/invalid-email":
+          toast.error("Invalid email format.");
+          break;
+        case "auth/invalid-credential":
+          toast.error("Invalid Credentials.");
+          break;
+        default:
+          toast.error("Login failed. Please try again.");
+      }
+    }
+  };
   return (
     <div className="container">
       <div className="card">
         <h2>Login</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label>Email</label>
             <input
@@ -31,6 +67,11 @@ const Login = () => {
           </div>
           <button type="submit">Login</button>
         </form>
+        <p>
+          <span style={{ color: "blue", cursor: "pointer" }}>
+            Forgot password?
+          </span>
+        </p>
         <p>
           Don't have an account? <Link to="/signup">Sign Up</Link>
         </p>

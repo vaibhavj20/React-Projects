437838e0824ff9f347e03fe3bb6c7a5c23f3b104
Vaibhav <vaibhavjam86@gmail.com>
2024-08-02 09:24:40 +0530

    created realtime database for storing full name and photo URL

--- Sharpener-Projects/Expense-Tracker/src/auth/firebase.js
+++ Sharpener-Projects/Expense-Tracker/src/auth/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,10 +13,12 @@ const firebaseConfig = {
   storageBucket: "expense-tracker-d7630.appspot.com",
   messagingSenderId: "675866649766",
   appId: "1:675866649766:web:92990b8e0fdb0b0a930067",
+  databaseURL: "https://expense-tracker-d7630-default-rtdb.firebaseio.com/",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const database = getDatabase(app);
 
-export { auth };
+export { auth, database };

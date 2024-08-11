66a5ad51e0591233c3c89060a1873832708640a9
Vaibhav <vaibhavjam86@gmail.com>
2024-08-11 10:36:49 +0530

    Added Expense list to show all added expenses by user

---
+++ Sharpener-Projects/Expense-Tracker/src/components/Expenses/ExpenseList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../styles/Expense.css"; // Import the updated CSS
+
+const ExpenseList = ({ expenses }) => {
+  return (
+    <div className="expense-list-container">
+      <h2>Expense List</h2>
+      <ul className="expense-list">
+        {expenses.map((expense, index) => (
+          <li key={index} className="expense-item">
+            <span>
+              {expense.description} - {expense.amount} ({expense.category})
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ExpenseList;

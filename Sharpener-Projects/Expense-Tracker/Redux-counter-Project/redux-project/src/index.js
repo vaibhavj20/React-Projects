929bffb665b3dfcd6f7fb1d4ad2ecdd6f946c09e
Vaibhav <vaibhavjam86@gmail.com>
2024-12-27 11:09:47 +0530

    auth added

--- Sharpener-Projects/Expense-Tracker/Redux-counter-Project/redux-project/src/index.js
+++ Sharpener-Projects/Expense-Tracker/Redux-counter-Project/redux-project/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "./store";
+
+import "./index.css";
 import App from "./App";
-import "./index.css"; // Add global styles
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
